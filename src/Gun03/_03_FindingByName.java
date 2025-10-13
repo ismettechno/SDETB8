0331803533bfd96ef98b466ea22a651b19197156
TechnoStudy <ismettechno@gmail.com>
2025-10-13 20:51:43 +0100

    _03_FindingByName

---
+++ src/Gun03/_03_FindingByName.java
@@ -0,0 +1,23 @@
+package Gun03;
+
+import Utility.MyFuc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _03_FindingByName {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://form.jotform.com/221934510376353");
+
+        WebElement ad=driver.findElement(By.name("q8_name[first]"));
+        ad.sendKeys("ismet");
+
+        WebElement soyad=driver.findElement(By.name("q8_name[last]"));
+        soyad.sendKeys("temur");
+
+        MyFuc.Bekle(3);
+        driver.quit();
+    }
+}
