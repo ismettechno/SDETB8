f1735356cb0eb53872361c0be1996dce7161d7ae
TechnoStudy <ismettechno@gmail.com>
2025-10-15 20:47:40 +0100

    _02_Soru

---
+++ src/Gun05/_01_Navigating.java
@@ -0,0 +1,37 @@
+package Gun05;
+
+import Utility.BaseDriver;
+import Utility.MyFuc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class _01_Navigating extends BaseDriver {
+
+    @Test
+    public void Test1()
+    {
+        driver.navigate().to("https://testpages.eviltester.com/styled/index.html");
+        // navigate().to -> get ile aynı, history için kullanılır, ileri - geri için
+        MyFuc.Bekle(3);
+
+        WebElement alertLink=driver.findElement(By.id("alerttest"));
+        alertLink.click();  // linke tıklattım yeni sayfayı açtım
+        MyFuc.Bekle(3);
+        System.out.println("Sayfanın title ı = " + driver.getTitle());
+        System.out.println("Sayfanın url si = " + driver.getCurrentUrl());
+
+        driver.navigate().back(); // back: geri,  bir önceki sayfaya git
+        MyFuc.Bekle(3);
+        System.out.println("Sayfanın title ı = " + driver.getTitle());
+        System.out.println("Sayfanın url si = " + driver.getCurrentUrl());
+
+        driver.navigate().forward(); // forward:ileri ,   bir sonraki sayfaya git.
+        MyFuc.Bekle(3);
+        System.out.println("Sayfanın title ı = " + driver.getTitle());
+        System.out.println("Sayfanın url si = " + driver.getCurrentUrl());
+
+        BekleKapat();
+    }
+
+}
