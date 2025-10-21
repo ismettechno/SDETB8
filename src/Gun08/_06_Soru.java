0d8a3494fc7779416c3d2d19cfb2705603e87d74
TechnoStudy <ismettechno@gmail.com>
2025-10-21 21:50:00 +0100

    _06_Soru

---
+++ src/Gun08/_06_Soru.java
@@ -0,0 +1,12 @@
+package Gun08;
+
+public class _06_Soru {
+
+//        Test Senaryosu
+//        1- https://www.facebook.com/  sayfasına gidiniz
+//        2- CreateNewAccount a tıklatınız.
+//        3- Açılan yeni pencerede isim, soyisim ve emaili giriniz.
+//        4- Doğum tarihinizi Select sınıfı ile seçiniz
+
+
+}
