a9af62e4decca9604456d43cc384aa4742f83a82
TechnoStudy <ismettechno@gmail.com>
2025-10-21 20:22:47 +0100

    _07_Soru çözüm Consent

--- src/Gun07/_07_Soru.java
+++ src/Gun07/_07_Soru.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.List;
+
 public class _07_Soru extends BaseDriver {
     //    https://www.hepsiburada.com.tr/ sayfasına gidiniz.
     //    Moda -> Pantolon  click
@@ -19,6 +21,15 @@ public class _07_Soru extends BaseDriver {
     public void Test1() {
         driver.get("https://www.hepsiburada.com/");
         MyFunc.Bekle(2);
+
+//        // consent ve cookies kapatmaları
+//        List<WebElement> ConsentButton=driver.findElements(By.xpath("//*[text()='Consent']"));
+//        if (ConsentButton.size()>0) // Consent ekranda gözüktüyse
+//            ConsentButton.get(0).click();
+//
+//        List<WebElement> KabulEtButton=driver.findElements(By.xpath("//*[text()='Kabul et']"));
+//        if (KabulEtButton.size()>0) // KabulEtButton u ekranda gözüktüyse
+//            KabulEtButton.get(0).click();
 
         WebElement moda=driver.findElement(By.xpath("//*[text()='Moda']"));
         new Actions(driver).moveToElement(moda).build().perform();
